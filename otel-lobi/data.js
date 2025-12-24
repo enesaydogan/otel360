ae5fde5a9aace6d6e143646991986a29f3f8859d
enesaydogan <enesaydogan@hotmail.com>
2025-12-24 17:04:59 +0300

    Update data.js

--- otel-lobi/data.js
+++ otel-lobi/data.js
@@ -24,7 +24,38 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -2.299651285811784,
+          "pitch": -0.006371255126790487,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        },
+        {
+          "yaw": -1.5426612843357663,
+          "pitch": 0.027009712466831814,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        },
+        {
+          "yaw": -2.8857750021040154,
+          "pitch": 0.034781842028374754,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": 2.736768714005229,
+          "pitch": 0.07214037451332445,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": 1.6602116530827145,
+          "pitch": 0.03162499251148354,
+          "rotation": 0,
+          "target": "5-sgara-len-alan"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -51,7 +82,38 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 2.045291157286181,
+          "pitch": 0.04766168708222729,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        },
+        {
+          "yaw": 2.5007678363297092,
+          "pitch": 0.02758002728626785,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        },
+        {
+          "yaw": -2.739934716727463,
+          "pitch": 0.020801494214820693,
+          "rotation": 0,
+          "target": "0-restaurant"
+        },
+        {
+          "yaw": -1.766952361746199,
+          "pitch": 0.07955874438739485,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": -2.2191396101598464,
+          "pitch": -0.011092114545478537,
+          "rotation": 0,
+          "target": "5-sgara-len-alan"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -78,7 +140,38 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.9573051790842211,
+          "pitch": 0.031822128008851536,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": 1.6736157093103605,
+          "pitch": -0.0229245844493029,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        },
+        {
+          "yaw": 2.006926685980722,
+          "pitch": 0.02462782076055703,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        },
+        {
+          "yaw": 2.91324192608076,
+          "pitch": 0.008993299544174604,
+          "rotation": 0,
+          "target": "0-restaurant"
+        },
+        {
+          "yaw": -1.6268834677643795,
+          "pitch": 0.07470952587565804,
+          "rotation": 0,
+          "target": "5-sgara-len-alan"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -105,7 +198,38 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 2.8869698643932447,
+          "pitch": 0.014405235606840705,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        },
+        {
+          "yaw": -0.70614564702068,
+          "pitch": 0.06315831886400503,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": -1.2882301789825021,
+          "pitch": 0.02232569110252136,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": -2.232134929849984,
+          "pitch": 0.013338984821649191,
+          "rotation": 0,
+          "target": "0-restaurant"
+        },
+        {
+          "yaw": -1.6025631364750996,
+          "pitch": -0.010495949177029118,
+          "rotation": 0,
+          "target": "5-sgara-len-alan"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -132,7 +256,38 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.34467631030290136,
+          "pitch": 0.042951057453107566,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        },
+        {
+          "yaw": -0.5703096647071337,
+          "pitch": 0.045916351808562794,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": -1.010603188003227,
+          "pitch": 0.04594076514844936,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": -1.847860376761421,
+          "pitch": 0.027980227881350572,
+          "rotation": 0,
+          "target": "0-restaurant"
+        },
+        {
+          "yaw": -1.4995681688681834,
+          "pitch": -0.008818537254432712,
+          "rotation": 0,
+          "target": "5-sgara-len-alan"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -159,7 +314,38 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.7210842208336459,
+          "pitch": 0.070723182129246,
+          "rotation": 0,
+          "target": "0-restaurant"
+        },
+        {
+          "yaw": -2.163374521677973,
+          "pitch": 0.10931357699852917,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": -1.9046794748417444,
+          "pitch": 0.07607464683892218,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": -1.4493364154258508,
+          "pitch": 0.0553745259299383,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        },
+        {
+          "yaw": -1.129779133339973,
+          "pitch": 0.047970203844718284,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        }
+      ],
       "infoHotspots": []
     }
   ],
