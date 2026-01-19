b3d708cee3ccc0089e108e98e5047700c66986f1
enesaydogan <enesaydogan@hotmail.com>
2026-01-19 10:05:55 +0300

    lobi revize panorama renderlar

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ Render 3 = https://enesaydogan.github.io/otel360/otel-ahsap/
 
 Render 4 = https://enesaydogan.github.io/otel360/otel-tas/
 
-Render o = https://enesaydogan.github.io/otel360/otel-lobi/
+Render lobi = https://enesaydogan.github.io/otel360/otel-lobi/
+
+Render lobi revize = https://enesaydogan.github.io/otel360/otel-lobi2/
---
+++ otel-lobi2/data.js
@@ -0,0 +1,353 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-restaurant",
+      "name": "RESTAURANT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5000990859537353,
+          "pitch": 0.04152114767517112,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        },
+        {
+          "yaw": 0.8809239073576158,
+          "pitch": 0.00852357017861749,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        },
+        {
+          "yaw": 0.19056990252294526,
+          "pitch": 0.032761727238478144,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": -0.28074695402800387,
+          "pitch": 0.024549423386249103,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": -1.0143896804246886,
+          "pitch": 0.0012854158811848038,
+          "rotation": 0,
+          "target": "5-sgara-len-alan"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hol",
+      "name": "HOL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.826072703161998,
+          "pitch": 0.0034622835358124604,
+          "rotation": 0,
+          "target": "0-restaurant"
+        },
+        {
+          "yaw": -1.5570263497694476,
+          "pitch": 0.07606976219021888,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": -1.78592437389654,
+          "pitch": 0.0035917224150843197,
+          "rotation": 0,
+          "target": "5-sgara-len-alan"
+        },
+        {
+          "yaw": 2.6114079548123392,
+          "pitch": 0.0112712240696613,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        },
+        {
+          "yaw": 2.060273215682077,
+          "pitch": 0.00846371649000588,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kafeterya",
+      "name": "KAFETERYA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6423533773894867,
+          "pitch": -0.024238157059855325,
+          "rotation": 0,
+          "target": "5-sgara-len-alan"
+        },
+        {
+          "yaw": 2.8769748287633785,
+          "pitch": -0.004277321834093684,
+          "rotation": 0,
+          "target": "0-restaurant"
+        },
+        {
+          "yaw": 1.1610538088754723,
+          "pitch": 0.007180004521952554,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": 1.6629565761901706,
+          "pitch": 0.004996758303823512,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        },
+        {
+          "yaw": 2.0253603484215343,
+          "pitch": -0.0007700628111475538,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-resepsyon-kordor",
+      "name": "RESEPSİYON KORİDOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7084008949326535,
+          "pitch": 0.05339393987650709,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": -1.233390697104964,
+          "pitch": 0.02545772477381192,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": -1.7011286624067097,
+          "pitch": -0.013985913795513838,
+          "rotation": 0,
+          "target": "5-sgara-len-alan"
+        },
+        {
+          "yaw": -2.187898664240425,
+          "pitch": 0.04725362715784698,
+          "rotation": 0,
+          "target": "0-restaurant"
+        },
+        {
+          "yaw": 2.794761054771845,
+          "pitch": 0.026448692135318197,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-resepsyon",
+      "name": "RESEPSİYON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2552817255257729,
+          "pitch": 0.025303297041126882,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        },
+        {
+          "yaw": -0.4926686263899285,
+          "pitch": 0.029093031840174532,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": -0.8704892834781326,
+          "pitch": 0.019342181511504464,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": -1.6710676809748257,
+          "pitch": 0.00031130395988654413,
+          "rotation": 0,
+          "target": "0-restaurant"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sgara-len-alan",
+      "name": "SİGARA İÇİLEN ALAN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9880877956584637,
+          "pitch": 0.05184213490724687,
+          "rotation": 0,
+          "target": "2-kafeterya"
+        },
+        {
+          "yaw": 1.2020490893432196,
+          "pitch": 0.059099693005535414,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": 2.0427846249828123,
+          "pitch": 0.010757609593047945,
+          "rotation": 0,
+          "target": "0-restaurant"
+        },
+        {
+          "yaw": 1.876030834434065,
+          "pitch": 0.008295535025270695,
+          "rotation": 0,
+          "target": "4-resepsyon"
+        },
+        {
+          "yaw": 1.5327593509732633,
+          "pitch": 0.029911398611661255,
+          "rotation": 0,
+          "target": "3-resepsyon-kordor"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "OTEL LOBİ REVİZE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
